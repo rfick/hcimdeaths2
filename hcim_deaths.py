bb48c4420dc4dfc355829ced007f1d3127a3c160
rfick <rfick@ufl.edu>
2022-05-27 02:10:40 -0400

    add python scheduling

--- hcim_deaths.py
+++ hcim_deaths.py
@@ -1,60 +1,79 @@
 import pickle
+from datetime import datetime, timedelta
 from pathlib import Path
 from processPage import *
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-page_url = 'https://secure.runescape.com/m=hiscore_oldschool_hardcore_ironman/overall?table=0&page='
+def checkHiScores():
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	print('Beginning hiscores check at {}...'.format(current_time))
 
-# 25 players per page
-numPages = 80
+	page_url = 'https://secure.runescape.com/m=hiscore_oldschool_hardcore_ironman/overall?table=0&page='
 
-# Only track top 200 for each boss KC
-numPagesKC = 8
+	# 25 players per page
+	numPages = 80
 
-#Order in which stats appear in the HTML table
-statOrder = ["overall", "attack", "defence", "strength", "hitpoints", "ranged", "prayer", "magic", "cooking", \
-			 "woodcutting", "fletching", "fishing", "firemaking", "crafting", "smithing", "mining", "herblore", \
-			 "agility", "thieving", "slayer", "farming", "runecraft", "hunter", "construction"]
-kcOrder = ["bountyHunterHunter", "bountyHunterRogue", "clueScrollsAll", "clueScrollsBeginner", "clueScrollsEasy", \
-		   "clueScrollsMedium", "clueScrollsHard", "clueScrollsElite", "clueScrollsMaster", "lms", "soulWarsZeal", \
-		   "riftsClosed", "abyssalSire", "alchemicalHydra", "barrowsChests", "bryophyta", "callisto", "cerberus", \
-		   "cox", "coxCM", "chaosElemental", "chaosFanatic", "commanderZilyana", "corporealBeast", "crazyArchaeologist", \
-		   "daggannothPrime", "daggannothRex", "daggannothSupreme", "derangedArchaeologist", "generalGraardor", \
-		   "giantMole", "grotesqueGuardians", "hespori", "kalphiteQueen", "kingBlackDragon", "kraken", "kreearra", \
-		   "krilTsutsaroth", "mimic", "nex", "nightmare", "phosanisNightmare", "obor", "sarachnis", "scorpia", \
-		   "skotizo", "tempoross", "gauntlet", "corruptedGauntlet", "tob", "tobHM", "thermonuclearSmokeDevil", \
-		   "tzKalZuk", "tzTokJad", "venenatis", "vetion", "vorkath", "wintertodt", "zalcano", "zulrah"]
-kcDispNames = ["Bounty Hunter - Hunter", "Bounty Hunter - Rogue", "Clue Scrolls - All", "Clue Scrolls - Beginner", "Clue Scrolls - Easy", \
-		   "Clue Scrolls - Medium", "Clue Scrolls - Hard", "Clue Scrolls - Elite", "Clue Scrolls - Master", "LMS", "Soul Wars Zeal", \
-		   "Rifts Closed", "Abyssal Sire", "Alchemical Hydra", "Barrows Chests", "Bryophyta", "Callisto", "Cerberus", \
-		   "COX", "COX CM", "Chaos Elemental", "Chaos Fanatic", "Commander Zilyana", "Corporeal Beast", "Crazy Archaeologist", \
-		   "Daggannoth Prime", "Daggannoth Rex", "Daggannoth Supreme", "Deranged Archaeologist", "General Graardor", \
-		   "Giant Mole", "Grotesque Guardians", "Hespori", "Kalphite Queen", "King Black Dragon", "Kraken", "Kree'arra", \
-		   "K'ril Tsutsaroth", "Mimic", "Nex", "Nightmare", "Phosani's Nightmare", "Obor", "Sarachnis", "Scorpia", \
-		   "Skotizo", "Tempoross", "Gauntlet", "Corrupted Gauntlet", "TOB", "TOB HM", "Thermonuclear Smoke Devil", \
-		   "Inferno", "Fight Caves", "Venenatis", "Vet'ion", "Vorkath", "Wintertodt", "Zalcano", "Zulrah"]
+	# Only track top 200 for each boss KC
+	numPagesKC = 8
 
-outFileName = 'trackedPlayers.pkl'
-# If dicitonary of tracked players doesn't exist create a new blank one
-if Path(outFileName).is_file():
-	with open(outFileName, 'rb') as f:
-		trackedPlayers = pickle.load(f)
-else:
-	trackedPlayers = {}
+	#Order in which stats appear in the HTML table
+	statOrder = ["overall", "attack", "defence", "strength", "hitpoints", "ranged", "prayer", "magic", "cooking", \
+				 "woodcutting", "fletching", "fishing", "firemaking", "crafting", "smithing", "mining", "herblore", \
+				 "agility", "thieving", "slayer", "farming", "runecraft", "hunter", "construction"]
+	kcOrder = ["bountyHunterHunter", "bountyHunterRogue", "clueScrollsAll", "clueScrollsBeginner", "clueScrollsEasy", \
+			   "clueScrollsMedium", "clueScrollsHard", "clueScrollsElite", "clueScrollsMaster", "lms", "soulWarsZeal", \
+			   "riftsClosed", "abyssalSire", "alchemicalHydra", "barrowsChests", "bryophyta", "callisto", "cerberus", \
+			   "cox", "coxCM", "chaosElemental", "chaosFanatic", "commanderZilyana", "corporealBeast", "crazyArchaeologist", \
+			   "daggannothPrime", "daggannothRex", "daggannothSupreme", "derangedArchaeologist", "generalGraardor", \
+			   "giantMole", "grotesqueGuardians", "hespori", "kalphiteQueen", "kingBlackDragon", "kraken", "kreearra", \
+			   "krilTsutsaroth", "mimic", "nex", "nightmare", "phosanisNightmare", "obor", "sarachnis", "scorpia", \
+			   "skotizo", "tempoross", "gauntlet", "corruptedGauntlet", "tob", "tobHM", "thermonuclearSmokeDevil", \
+			   "tzKalZuk", "tzTokJad", "venenatis", "vetion", "vorkath", "wintertodt", "zalcano", "zulrah"]
+	kcDispNames = ["Bounty Hunter - Hunter", "Bounty Hunter - Rogue", "Clue Scrolls - All", "Clue Scrolls - Beginner", "Clue Scrolls - Easy", \
+			   "Clue Scrolls - Medium", "Clue Scrolls - Hard", "Clue Scrolls - Elite", "Clue Scrolls - Master", "LMS", "Soul Wars Zeal", \
+			   "Rifts Closed", "Abyssal Sire", "Alchemical Hydra", "Barrows Chests", "Bryophyta", "Callisto", "Cerberus", \
+			   "COX", "COX CM", "Chaos Elemental", "Chaos Fanatic", "Commander Zilyana", "Corporeal Beast", "Crazy Archaeologist", \
+			   "Daggannoth Prime", "Daggannoth Rex", "Daggannoth Supreme", "Deranged Archaeologist", "General Graardor", \
+			   "Giant Mole", "Grotesque Guardians", "Hespori", "Kalphite Queen", "King Black Dragon", "Kraken", "Kree'arra", \
+			   "K'ril Tsutsaroth", "Mimic", "Nex", "Nightmare", "Phosani's Nightmare", "Obor", "Sarachnis", "Scorpia", \
+			   "Skotizo", "Tempoross", "Gauntlet", "Corrupted Gauntlet", "TOB", "TOB HM", "Thermonuclear Smoke Devil", \
+			   "Inferno", "Fight Caves", "Venenatis", "Vet'ion", "Vorkath", "Wintertodt", "Zalcano", "Zulrah"]
 
-# 1 = Dead, 0 = Alive
-newTrackedPlayers = {}
+	outFileName = 'trackedPlayers.pkl'
+	# If dicitonary of tracked players doesn't exist create a new blank one
+	if Path(outFileName).is_file():
+		with open(outFileName, 'rb') as f:
+			trackedPlayers = pickle.load(f)
+	else:
+		trackedPlayers = {}
 
-processPage(page_url, numPages, statOrder, kcOrder, kcDispNames, trackedPlayers, newTrackedPlayers)
+	# 1 = Dead, 0 = Alive
+	newTrackedPlayers = {}
 
-# Check kc pages
-# table=13 -> Abyssal Sire
-# table=60 -> Zulrah
-firstKcTracked = 13
-lastKcTracked = 60
-for i in range(firstKcTracked, lastKcTracked+1):
-	print('Processing kc table {}/{}'.format(i-firstKcTracked, lastKcTracked-firstKcTracked))
-	kc_page_url = 'https://secure.runescape.com/m=hiscore_oldschool_hardcore_ironman/overall?category_type=1&table={}&page='.format(str(i))
-	processPage(kc_page_url, numPagesKC, statOrder, kcOrder, kcDispNames, trackedPlayers, newTrackedPlayers)
+	processPage(page_url, numPages, statOrder, kcOrder, kcDispNames, trackedPlayers, newTrackedPlayers)
 
-with open(outFileName, 'wb') as f:
-	pickle.dump(newTrackedPlayers, f)
+	# Check kc pages
+	# table=13 -> Abyssal Sire
+	# table=60 -> Zulrah
+	firstKcTracked = 13
+	lastKcTracked = 60
+	for i in range(firstKcTracked, lastKcTracked+1):
+		print('Processing kc table {}/{}'.format(i-firstKcTracked, lastKcTracked-firstKcTracked))
+		kc_page_url = 'https://secure.runescape.com/m=hiscore_oldschool_hardcore_ironman/overall?category_type=1&table={}&page='.format(str(i))
+		processPage(kc_page_url, numPagesKC, statOrder, kcOrder, kcDispNames, trackedPlayers, newTrackedPlayers)
+
+	with open(outFileName, 'wb') as f:
+		pickle.dump(newTrackedPlayers, f)
+
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	print('Hiscores check complete at {}.'.format(current_time))
+
+#Begin in 1 minute, update every 1.5 hrs after
+current_time = datetime.now()
+start_time = current_time + timedelta(minutes=1)
+
+scheduler = BlockingScheduler()
+scheduler.add_job(checkHiScores, 'interval', hours=1.5, start_date=start_time)
+scheduler.start()
